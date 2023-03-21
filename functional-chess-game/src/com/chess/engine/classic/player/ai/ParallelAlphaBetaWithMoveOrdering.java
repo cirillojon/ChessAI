9797e544a37e9767a2804e72163df9c683cb501d
cirillojon <104590083+cirillojon@users.noreply.github.com>
2023-03-21 09:43:27 -0400

    Added Fork/Join & Parallel Streams implementations
    Added Fork/Join & Parallel Streams multithreaded implementations. We can use these to test and see which performs the best.

--- functional-chess-game/src/com/chess/engine/classic/player/ai/ParallelAlphaBetaWithMoveOrdering.java
+++ functional-chess-game/src/com/chess/engine/classic/player/ai/ParallelAlphaBetaWithMoveOrdering.java
@@ -17,18 +17,23 @@ import java.util.List;
 
 // AtomicInteger is used for thread-safe operations on integers.
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 // ConcurrentHashMap is added for thread-safe use as a transposition table.
 import java.util.concurrent.ConcurrentHashMap;
 
-// Necessary imports to implement parellelism
+// Necessary imports to implement parellelism via executor service
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
-public class ParallelAlphaBetaWithMoveOrdering extends Observable implements MoveStrategy {
+// Necessary imports to implement parellelism via fork-join task
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+import java.util.stream.Collectors;
 
+public class ParallelAlphaBetaWithMoveOrdering extends Observable implements MoveStrategy {
     private final BoardEvaluator evaluator;
     private final int searchDepth;
     private final MoveSorter moveSorter;
@@ -37,7 +42,7 @@ public class ParallelAlphaBetaWithMoveOrdering extends Observable implements Mov
     private int quiescenceCount;
     private int cutOffsProduced;
 
-    // Added: Transposition table to store previously computed values.
+    // Transposition table to store previously computed values.
     private final ConcurrentHashMap<Long, Integer> transpositionTable = new ConcurrentHashMap<>();
 
     private enum MoveSorter {
@@ -83,6 +88,107 @@ public class ParallelAlphaBetaWithMoveOrdering extends Observable implements Mov
         return this.boardsEvaluated;
     }
 
+    //Paralleism using ForkJoinTask which utilizes work stealing
+    /* 
+    @Override
+    public Move execute(final Board board) {
+        final long startTime = System.currentTimeMillis();
+
+        final Player currentPlayer = board.currentPlayer();
+        final Alliance alliance = currentPlayer.getAlliance();
+        final AtomicReference<Move> bestMove = new AtomicReference<>(MoveFactory.getNullMove());
+
+        AtomicInteger highestSeenValue = new AtomicInteger(Integer.MIN_VALUE);
+        AtomicInteger lowestSeenValue = new AtomicInteger(Integer.MAX_VALUE);
+        AtomicInteger moveCounter = new AtomicInteger(1);
+
+        final int numMoves = this.moveSorter.sort(board.currentPlayer().getLegalMoves()).size();
+        System.out.println(board.currentPlayer() + " THINKING with depth = " + this.searchDepth);
+        System.out.println("\tOrdered moves! : " + this.moveSorter.sort(board.currentPlayer().getLegalMoves()));
+
+        // Create tasks
+        List<ForkJoinTask<MoveResult>> tasks = new ArrayList<>();
+
+        for (final Move move : this.moveSorter.sort(board.currentPlayer().getLegalMoves())) {
+            tasks.add(new MoveResultTask(move, highestSeenValue, lowestSeenValue, moveCounter, numMoves, board, alliance, bestMove));
+        }
+
+        List<MoveResult> results = RecursiveTask.invokeAll(tasks).stream().map(ForkJoinTask::join).collect(Collectors.toList());
+
+
+        // Process results
+        for (MoveResult moveResult : results) {
+            Move move = moveResult.move;
+            int currentValue = moveResult.value;
+
+            if (alliance.isWhite() && currentValue > highestSeenValue.get()) {
+                highestSeenValue.set(currentValue);
+                bestMove.set(move);
+            } else if (alliance.isBlack() && currentValue < lowestSeenValue.get()) {
+                lowestSeenValue.set(currentValue);
+                bestMove.set(move);
+            }
+        }
+
+        this.executionTime = System.currentTimeMillis() - startTime;
+        System.out.printf("%s SELECTS %s [#boards evaluated = %d, time taken = %d ms, eval rate = %.1f cutoffCount = %d prune percent = %.2f\n", board.currentPlayer(),
+                bestMove.get().toString(), this.boardsEvaluated, this.executionTime, (1000 * ((double) this.boardsEvaluated / this.executionTime)), this.cutOffsProduced, 100 * ((double) this.cutOffsProduced / this.boardsEvaluated));
+        return bestMove.get();
+    }
+
+    // Define the inner class MoveResultTask to implement the fork-join task
+    private class MoveResultTask extends RecursiveTask<MoveResult> {
+        private final Move move;
+        private final AtomicInteger highestSeenValue;
+        private final AtomicInteger lowestSeenValue;
+        private final AtomicInteger moveCounter;
+        private final AtomicReference<Move> bestMove;
+        private final int numMoves;
+        private final Board board;
+        private final Alliance alliance;
+
+        public MoveResultTask(Move move, AtomicInteger highestSeenValue, AtomicInteger lowestSeenValue,
+                              AtomicInteger moveCounter, int numMoves, Board board, Alliance alliance, AtomicReference<Move> bestMove) {
+            this.move = move;
+            this.highestSeenValue = highestSeenValue;
+            this.lowestSeenValue = lowestSeenValue;
+            this.moveCounter = moveCounter;
+            this.numMoves = numMoves;
+            this.board = board;
+            this.alliance = alliance;
+            this.bestMove = bestMove;
+        }
+
+        @Override
+        protected MoveResult compute() {
+            final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+
+            if (moveTransition.getMoveStatus().isDone()) {
+                final long candidateMoveStartTime = System.nanoTime();
+                int currentValue = alliance.isWhite() ?
+                        min(moveTransition.getToBoard(), searchDepth - 1, highestSeenValue.get(), lowestSeenValue.get()) :
+                        max(moveTransition.getToBoard(), searchDepth - 1, highestSeenValue.get(), lowestSeenValue.get());
+
+                final String quiescenceInfo = " [h: " + highestSeenValue + " l: " + lowestSeenValue + "] q: " + quiescenceCount;
+                final String s = "\t" + toString() + "(" + searchDepth + "), m: (" + moveCounter.getAndIncrement() + "/" + numMoves + ") " + move + ", best:  " + bestMove.get().toString()
+                        + quiescenceInfo + ", t: " + calculateTimeTaken(candidateMoveStartTime, System.nanoTime()) + ", thread: " + Thread.currentThread().getName();
+
+
+                System.out.println(s);
+
+                setChanged();
+                notifyObservers(s);
+
+                return new MoveResult(move, currentValue);
+            } else {
+                return new MoveResult(move, alliance.isWhite() ? Integer.MIN_VALUE : Integer.MAX_VALUE);
+            }
+        }
+    }
+
+    */
+
+    //Parellism  using ExecutorService  
     @Override
     public Move execute(final Board board) {
         final long startTime = System.currentTimeMillis();
@@ -160,12 +266,70 @@ public class ParallelAlphaBetaWithMoveOrdering extends Observable implements Mov
                 bestMove[0], this.boardsEvaluated, this.executionTime, (1000 * ((double) this.boardsEvaluated / this.executionTime)), this.cutOffsProduced, 100 * ((double) this.cutOffsProduced / this.boardsEvaluated));
         return bestMove[0];
     }
+    
+
+    /* 
+    // Parallelism using Parallel Streams
+    @Override
+    public Move execute(final Board board) {
+        final long startTime = System.currentTimeMillis();
+
+        final Player currentPlayer = board.currentPlayer();
+        final Alliance alliance = currentPlayer.getAlliance();
+
+        AtomicInteger highestSeenValue = new AtomicInteger(Integer.MIN_VALUE);
+        AtomicInteger lowestSeenValue = new AtomicInteger(Integer.MAX_VALUE);
+        AtomicInteger moveCounter = new AtomicInteger(1);
+
+        final int numMoves = this.moveSorter.sort(board.currentPlayer().getLegalMoves()).size();
+        System.out.println(board.currentPlayer() + " THINKING with depth = " + this.searchDepth);
+        System.out.println("\tOrdered moves! : " + this.moveSorter.sort(board.currentPlayer().getLegalMoves()));
+
+        Move bestMove = this.moveSorter.sort(board.currentPlayer().getLegalMoves()).parallelStream()
+                .map(move -> {
+                    final MoveTransition moveTransition = board.currentPlayer().makeMove(move);
+                    this.quiescenceCount = 0;
+                    if (moveTransition.getMoveStatus().isDone()) {
+                        final long candidateMoveStartTime = System.nanoTime();
+                        int currentValue = alliance.isWhite() ?
+                                min(moveTransition.getToBoard(), this.searchDepth - 1, highestSeenValue.get(), lowestSeenValue.get()) :
+                                max(moveTransition.getToBoard(), this.searchDepth - 1, highestSeenValue.get(), lowestSeenValue.get());
+
+                        final String quiescenceInfo = " [h: " + highestSeenValue + " l: " + lowestSeenValue + "] q: " + this.quiescenceCount;
+                        final String s = "\t" + toString() + "(" + this.searchDepth + "), m: (" + moveCounter.getAndIncrement() + "/" + numMoves + ") " + move
+                        + quiescenceInfo + ", t: " + calculateTimeTaken(candidateMoveStartTime, System.nanoTime()) + ", thread: " + Thread.currentThread().getName();
 
 
-    public int max(final Board board,
-                   final int depth,
-                   final int highest,
-                   final int lowest) {
+                        System.out.println(s);
+
+                        setChanged();
+                        notifyObservers(s);
+
+                        return new MoveResult(move, currentValue);
+                    } else {
+                        return new MoveResult(move, alliance.isWhite() ? Integer.MIN_VALUE : Integer.MAX_VALUE);
+                    }
+                })
+                .reduce((result1, result2) -> {
+                    if (alliance.isWhite()) {
+                        return result1.value > result2.value ? result1 : result2;
+                    } else {
+                        return result1.value < result2.value ? result1 : result2;
+                    }
+                })
+                .map(result -> result.move)
+                .orElse(MoveFactory.getNullMove());
+
+                this.executionTime = System.currentTimeMillis() - startTime;
+                System.out.printf("%s SELECTS %s [#boards evaluated = %d, time taken = %d ms, eval rate = %.1f cutoffCount = %d prune percent = %.2f\n", board.currentPlayer(),
+                        bestMove, this.boardsEvaluated, this.executionTime, (1000 * ((double) this.boardsEvaluated / this.executionTime)), this.cutOffsProduced, 100 * ((double) this.cutOffsProduced / this.boardsEvaluated));
+                System.out.println("Best move: " + bestMove);
+                return bestMove;
+        }
+        */
+     
+    public int max(final Board board, final int depth, final int highest, final int lowest) {
+
         if (depth == 0 || BoardUtils.isEndGame(board)) {
             this.boardsEvaluated++;
             return this.evaluator.evaluate(board, depth);
@@ -202,10 +366,8 @@ public class ParallelAlphaBetaWithMoveOrdering extends Observable implements Mov
         return currentHighest;
     }
 
-    public int min(final Board board,
-                   final int depth,
-                   final int highest,
-                   final int lowest) {
+    public int min(final Board board, final int depth, final int highest, final int lowest) {
+
         if (depth == 0 || BoardUtils.isEndGame(board)) {
             this.boardsEvaluated++;
             return this.evaluator.evaluate(board, depth);
@@ -234,9 +396,7 @@ public class ParallelAlphaBetaWithMoveOrdering extends Observable implements Mov
         return currentLowest;
     }
 
-    private int calculateQuiescenceDepth(final Board board,
-                                         final Move move,
-                                         final int depth) {
+    private int calculateQuiescenceDepth(final Board board, final Move move, final int depth) {
         return depth - 1;
     }
 
